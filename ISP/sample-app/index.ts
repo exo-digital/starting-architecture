69bd3cdd2b60342381cb99d8ab476d224d87e4e6
Exo Digital <exodigital@Exos-MBP.gateway>
2021-07-06 12:47:19 +1000

    added ISP

---
+++ ISP/sample-app/index.ts
@@ -0,0 +1,40 @@
+class BankAccount {
+  constructor(
+    amount: number,
+    user: User,
+    openDate: Date,
+    lastTransactionAmmount: number
+  ) {
+    this.amount = amount;
+    this.user = user;
+    this.openDate = openDate;
+    this.lastTransactionAmmount = lastTransactionAmmount;
+  }
+  amount: number;
+  user: User;
+  openDate: Date;
+  lastTransactionAmmount: number;
+}
+class User {
+  constructor(name: string) {
+    this.name = name;
+  }
+  name: string;
+}
+
+function calculateTotalBankMoney(accounts: BankAccount[]): number {
+  var sum = 0;
+  accounts.forEach((account) => {
+    sum += account.amount;
+  });
+
+  return sum;
+}
+const accounts = [
+  new BankAccount(1, new User("Jack"), new Date("01/01/2021"), 20),
+  new BankAccount(30, new User("John"), new Date("05/01/1900"), 1),
+  new BankAccount(4000, new User("Mary"), new Date("01/02/1984"), 30),
+];
+const totalMoney = calculateTotalBankMoney(accounts);
+
+console.log(`Here are the total bank money : $${totalMoney}`);
